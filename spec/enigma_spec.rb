a2a4e0465ff4a9ff66f6011a871ab7a9fc36be6f
Christina Delpone <cdelpone@gmail.com>
2021-06-13 15:43:01 -0600

    Avoid git-tastrophe

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+RSpec.describe Enigma do
+
+  it 'exists' do
+    enigma = Enigma.new
+
+    expect(enigma).to be_an Enigma
+  end
+
+  xit 'encrypts' do
+    enigma = Enigma.new
+
+    expected = {
+               encryption: "keder ohulw",
+               key: "02715",
+               date: "040895"
+             }
+
+    expect(enigma.encrypt('hello world', '02715', '040895')).to eq(expected)
+  end
+
+  xit 'decrypts' do
+    enigma = Enigma.new
+
+    expected = {
+               encryption: "hello world",
+               key: "02715",
+               date: "040895"
+             }
+
+    expect(enigma.decrypt('keder ohulw', '02715', '040895')).to eq(expected)
+  end
+end
