fa3d51f90cc0a33c78eb2344c6b2b6ac89b365db
Christina Delpone <cdelpone@gmail.com>
2021-06-12 18:38:34 -0600

    create enigma class and tests

---
+++ lib/enigma.rb
@@ -0,0 +1,42 @@
+class Enigma
+
+  attr_reader :encrypt
+
+  def initialize
+    @encrypt = Hash.new
+    @decrypt = Hash.new
+
+    # @encryption = encrypt[:encryption]
+    # @key = encrypt[:key]
+    # @date = encrypt[:date]
+  end
+
+  def encrypt(message, key, date)
+    @encrypt = {}
+    if @encrypt[:encryption].nil?
+      @encrypt[:encryption] = message
+    end
+    if @encrypt[:key].nil?
+      @encrypt[:key] = key
+    end
+    if @encrypt[:date].nil?
+      @encrypt[:date] = date
+    end
+    @encrypt
+  end
+
+  def decrypt(message, key, date)
+    @decrypt = {}
+    if @decrypt[:decryption].nil?
+      @decrypt[:decryption] = message
+    end
+    if @decrypt[:key].nil?
+      @decrypt[:key] = key
+    end
+    if @decrypt[:date].nil?
+      @decrypt[:date] = date
+    end
+    @decrypt
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'simplecov'
+
+SimpleCov.start
+
+require './lib/enigma'
+require './lib/shift_generator'
