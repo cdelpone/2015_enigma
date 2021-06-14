f13a66db7ad7b90653d121b5a40628efbceeb9cc
Christina Delpone <cdelpone@gmail.com>
2021-06-14 11:20:06 -0600

    Create shift generator class and tests

--- spec/shift_generator_spec.rb
+++ spec/shift_generator_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe ShiftGenerator do
     expect(shift.random_nums.length).to eq 5
   end
 
-  it 'returns keys as integers' do
+  xit 'returns keys as integers in a hash' do
     shift = ShiftGenerator.new
 
     expect(shift.assign_keys.integer?).to eq true
@@ -23,6 +23,31 @@ RSpec.describe ShiftGenerator do
   it 'returns todays date as DDMMYY' do
     shift = ShiftGenerator.new
 
-    expect(shift.convert_today_date).to eq('130621')
+    expect(shift.convert_today_date).to eq('140621')
+  end
+
+  it 'squares transmission date' do
+    shift = ShiftGenerator.new
+
+    expect(shift.transmission_date).to eq('19636441')
+  end
+
+  it 'returns and assigns offsets' do
+    shift = ShiftGenerator.new
+
+    expected = a_offset = 6,
+              b_offset = 4,
+              c_offset = 4,
+              d_offset = 1
+
+    expect(shift.assign_offsets).to eq(expected)
+  end
+
+  it 'can create shifts' do
+    shift = ShiftGenerator.new
+    combo_keys = [18, 82, 24, 49]
+    combo_offsets = [6, 4, 4, 1]
+
+    expect(shift.create_shifts(combo_keys, combo_offsets)).to eq([24, 86, 28, 50])
   end
 end
